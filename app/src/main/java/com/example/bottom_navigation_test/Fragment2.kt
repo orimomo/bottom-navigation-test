1e31d2500cc5d51f3c4f3ac4bbb1c179575e1783
momo <mmko5110@gmail.com>
2019-06-16 17:33:29 +0900

    fragmentファイルを4つ作成

---
+++ app/src/main/java/com/example/bottom_navigation_test/Fragment2.kt
@@ -0,0 +1,14 @@
+package com.example.bottom_navigation_test
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment2 : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return  inflater.inflate(R.layout.fragment_2, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/bottom_navigation_test/Fragment4.kt
@@ -0,0 +1,14 @@
+package com.example.bottom_navigation_test
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment4 : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return  inflater.inflate(R.layout.fragment_4, container, false)
+    }
+}
